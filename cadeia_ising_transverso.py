0ebfbce89a293a3b1b39ac875f7bdc85d8e3ac53
vdvdlk <thalesfreitas96@gmail.com>
2022-12-26 04:19:37 -0300

    As autoenergias estão batendo com o livro

--- cadeia_ising_transverso.py
+++ cadeia_ising_transverso.py
@@ -8,27 +8,30 @@ def imprimir_base(N: int):
     base = ['+', '-']
     array_indices = 2 * ones(N, dtype=int)
 
+    i = 0
     for indices in ndindex(tuple(array_indices)):
         string = ''
         for indice in indices:
             string += base[indice]
-        print('|' + string + '>')
+        print(i, '|' + string + '>')
+        i += 1
 
 
 def S_i_x(N: int, i: int) -> ndarray:
     S_x = array(
-        object=[[0, 1], [1, 0]],
-        dtype=float,
+        # object=[[0, 1], [1, 0]],  # base z
+        object=[[1, 0], [0, -1]],  # base x
+        dtype=complex,
     )
 
     I = identity(
         n=2,
-        dtype=float,
+        dtype=complex,
     )
 
     matriz = identity(
         n=1,
-        dtype=float,
+        dtype=complex,
     )
 
     for j in arange(1, N + 1):
@@ -47,20 +50,21 @@ def S_i_x(N: int, i: int) -> ndarray:
 
 
 def S_i_z(N: int, i: int) -> ndarray:
-    S_z = array([[1, 0], [0, -1]])
+    un_im = complex(0, 1)
     S_z = array(
-        object=[[1, 0], [0, -1]],
-        dtype=float,
+        # object=[[1, 0], [0, -1]],  # base z
+        object=[[0, 1], [1, 0]],  # base x
+        dtype=complex,
     )
 
     I = identity(
         n=2,
-        dtype=float,
+        dtype=complex,
     )
 
     matriz = identity(
         n=1,
-        dtype=float,
+        dtype=complex,
     )
 
     for j in arange(1, N + 1):
@@ -81,12 +85,12 @@ def S_i_z(N: int, i: int) -> ndarray:
 def hamiltoniana_N(N: int, lamda: float) -> ndarray:
     termo_int = zeros(
         shape=(2 ** N, 2 ** N),
-        dtype=float,
+        dtype=complex,
     )
 
     termo_campo = zeros(
         shape=(2 ** N, 2 ** N),
-        dtype=float,
+        dtype=complex,
     )
 
     for i in arange(1, (N - 1) + 1):
@@ -101,12 +105,41 @@ def hamiltoniana_N(N: int, lamda: float) -> ndarray:
     return -(termo_int + lamda * termo_campo)
 
 
-def diagonalizacao_N(N: int, lamda_min: float = 0.0, lamda_max: float = 2.0, num: int = 1000):
-    array_lamda = linspace(
-        start=lamda_min,
-        stop=lamda_max,
-        num=num,
+def hamiltoniana_N_ccp(N: int, lamda: float) -> ndarray:
+    termo_int = zeros(
+        shape=(2 ** N, 2 ** N),
+        dtype=complex,
     )
+
+    termo_campo = zeros(
+        shape=(2 ** N, 2 ** N),
+        dtype=complex,
+    )
+
+    I_N = identity(
+        n=2 ** N,
+        dtype=complex
+    )
+
+    for i in arange(1, (N - 1) + 1):
+        termo_int += matmul(
+            S_i_z(N=N, i=i),
+            S_i_z(N=N, i=i + 1),
+        )
+    
+    if N != 2:
+        termo_int += matmul(
+            S_i_z(N=N, i=N),
+            S_i_z(N=N, i=1)
+        )
+
+    for i in arange(1, N + 1):
+        termo_campo += I_N - S_i_x(N=N, i=i)
+
+    return termo_campo - lamda * termo_int
+
+
+def diagonalizacao_N(N: int, array_lamda: ndarray):
 
     autovalores = zeros(
         shape=(array_lamda.size, 2 ** N),
@@ -119,7 +152,7 @@ def diagonalizacao_N(N: int, lamda_min: float = 0.0, lamda_max: float = 2.0, num
     )
 
     for k in trange(array_lamda.size, desc='Diagonalização da cadeia de Ising transverso para ' + str(N) + ' sítios'):
-        H = hamiltoniana_N(
+        H = hamiltoniana_N_ccp(
             N=N,
             lamda=array_lamda[k],
         )
@@ -131,4 +164,4 @@ def diagonalizacao_N(N: int, lamda_min: float = 0.0, lamda_max: float = 2.0, num
         autovalores[k, :] = w
         autovetores[k, :, :] = v
 
-    return array_lamda, autovalores, autovetores
+    return autovalores, autovetores
--- projeto.py
+++ projeto.py
@@ -1,18 +1,51 @@
-from numpy import argwhere, diff, sign
-from matplotlib.pyplot import subplots, show
+from matplotlib.pyplot import show, subplots
+from numpy import argwhere, diff, save, load, sign, linspace, arange, ndarray
 
 from cadeia_ising_transverso import diagonalizacao_N
 
-
 # Diagonalização exata de cadeia finita
+
+# array_lamda = linspace(
+#     start=0.0,
+#     stop=10.0,
+#     num=1000,
+# )
+# save(
+#     file='projeto/array_lamda',
+#     arr=array_lamda,
+# )
+array_lamda = load(
+    file='projeto/array_lamda.npy'
+)
+
+
+# for N in arange(2, 9):
+#     autoval, autovet = diagonalizacao_N(
+#         N=N,
+#         array_lamda=array_lamda
+#     )
+#     save(
+#         file='projeto/autoval_' + str(N),
+#         arr=autoval,
+#     )
+#     save(
+#         file='projeto/autovet_' + str(N),
+#         arr=autovet,
+#     )
 
 
 def intersec_grafico(f, g):
     return argwhere(diff(sign(f - g))).flatten()
 
 
-def grafico_autoval_cf(N: int):
-    lamda, autoval, autovet = diagonalizacao_N(N=N)
+def grafico_autoval_cf(N: int, lamda: ndarray = array_lamda):
+    # autoval, autovet = diagonalizacao_N(
+    #     N=N,
+    # )
+
+    autoval = load(
+        file='projeto/autoval_' + str(N) + '.npy'
+    )
 
     fig, ax = subplots()
     ax.set_title('Autovalores em função de $\lambda$ (N = ' + str(N) + ')')
@@ -25,20 +58,51 @@ def grafico_autoval_cf(N: int):
     return fig
 
 
+def grafico_gap(N: int, lamda: ndarray = array_lamda):
+    # autoval, autovet = diagonalizacao_N(
+    #     N=N,
+    # )
+
+    autoval = load(
+        file='projeto/autoval_' + str(N) + '.npy'
+    )
+
+    fig, ax = subplots()
+    ax.set_title('Autovalores em função de $\lambda$ (N = ' + str(N) + ')')
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('Energia')
+
+    ax.plot(lamda, autoval[:, 1] - autoval[:, 0])
+
+    return fig
+
+
 # N = 2
-fig_2 = grafico_autoval_cf(2)
+fig_2 = grafico_gap(2)
+# fig_2 = grafico_autoval_cf(2)
+
+# N = 3
+fig_3 = grafico_gap(3)
+# fig_3 = grafico_autoval_cf(3)
 
 # N = 4
+fig_4 = grafico_gap(4)
+# fig_4 = grafico_autoval_cf(4)
 
-fig_4 = grafico_autoval_cf(4)
+# N = 5
+fig_5 = grafico_gap(5)
+# fig_5 = grafico_autoval_cf(5)
 
 # N = 6
+fig_6 = grafico_gap(6)
+# fig_6 = grafico_autoval_cf(6)
 
-fig_6 = grafico_autoval_cf(6)
+# N = 7
+fig_7 = grafico_gap(7)
+# fig_7 = grafico_autoval_cf(7)
 
-# N = 8  (5 min)
-
-fig_8 = grafico_autoval_cf(8)
-
+# N = 8
+fig_8 = grafico_gap(8)
+# fig_8 = grafico_autoval_cf(8)
 
 show()

33ac8b36c36be6681e1146679f5144951e72bcbf
vdvdlk <thalesfreitas96@gmail.com>
2022-12-26 22:20:56 -0300

    Incluí os arquivos dos estados de energia

--- cadeia_ising_transverso.py
+++ cadeia_ising_transverso.py
@@ -1,7 +1,9 @@
 from numpy import (arange, array, identity, kron, linspace, matmul, ndarray,
-                   ndindex, zeros, ones)
-from numpy.linalg import eigh
+                   ndindex, ones, save, zeros)
+from numpy.linalg import eigvalsh
 from tqdm.auto import trange
+
+N_max = 10
 
 
 def imprimir_base(N: int):
@@ -50,7 +52,7 @@ def S_i_x(N: int, i: int) -> ndarray:
 
 
 def S_i_z(N: int, i: int) -> ndarray:
-    un_im = complex(0, 1)
+    # un_im = complex(0, 1)
     S_z = array(
         # object=[[1, 0], [0, -1]],  # base z
         object=[[0, 1], [1, 0]],  # base x
@@ -82,7 +84,30 @@ def S_i_z(N: int, i: int) -> ndarray:
     return matriz
 
 
-def hamiltoniana_N(N: int, lamda: float) -> ndarray:
+# def hamiltoniana_N(N: int, lamda: float) -> ndarray:
+#     termo_int = zeros(
+#         shape=(2 ** N, 2 ** N),
+#         dtype=complex,
+#     )
+
+#     termo_campo = zeros(
+#         shape=(2 ** N, 2 ** N),
+#         dtype=complex,
+#     )
+
+#     for i in arange(1, (N - 1) + 1):
+#         termo_int += matmul(
+#             S_i_z(N=N, i=i),
+#             S_i_z(N=N, i=i + 1),
+#         )
+
+#     for i in arange(1, N + 1):
+#         termo_campo += S_i_x(N=N, i=i)
+
+#     return -(termo_int + lamda * termo_campo)
+
+
+def hamiltoniana_N(N: int, lamda: float, ccp: bool = True) -> ndarray:
     termo_int = zeros(
         shape=(2 ** N, 2 ** N),
         dtype=complex,
@@ -99,39 +124,16 @@ def hamiltoniana_N(N: int, lamda: float) -> ndarray:
             S_i_z(N=N, i=i + 1),
         )
 
-    for i in arange(1, N + 1):
-        termo_campo += S_i_x(N=N, i=i)
-
-    return -(termo_int + lamda * termo_campo)
-
-
-def hamiltoniana_N_ccp(N: int, lamda: float) -> ndarray:
-    termo_int = zeros(
-        shape=(2 ** N, 2 ** N),
-        dtype=complex,
-    )
-
-    termo_campo = zeros(
-        shape=(2 ** N, 2 ** N),
-        dtype=complex,
-    )
+    if N != 2 and ccp == True:
+        termo_int += matmul(
+            S_i_z(N=N, i=N),
+            S_i_z(N=N, i=1)
+        )
 
     I_N = identity(
         n=2 ** N,
         dtype=complex
     )
-
-    for i in arange(1, (N - 1) + 1):
-        termo_int += matmul(
-            S_i_z(N=N, i=i),
-            S_i_z(N=N, i=i + 1),
-        )
-    
-    if N != 2:
-        termo_int += matmul(
-            S_i_z(N=N, i=N),
-            S_i_z(N=N, i=1)
-        )
 
     for i in arange(1, N + 1):
         termo_campo += I_N - S_i_x(N=N, i=i)
@@ -142,26 +144,53 @@ def hamiltoniana_N_ccp(N: int, lamda: float) -> ndarray:
 def diagonalizacao_N(N: int, array_lamda: ndarray):
 
     autovalores = zeros(
-        shape=(array_lamda.size, 2 ** N),
+        # shape=(array_lamda.size, 2 ** N),
+        shape=(array_lamda.size, 2),
         dtype=float,
     )
 
-    autovetores = zeros(
-        shape=(array_lamda.size, 2 ** N, 2 ** N),
-        dtype=complex,
-    )
-
     for k in trange(array_lamda.size, desc='Diagonalização da cadeia de Ising transverso para ' + str(N) + ' sítios'):
-        H = hamiltoniana_N_ccp(
+        H = hamiltoniana_N(
             N=N,
             lamda=array_lamda[k],
         )
 
-        w, v = eigh(
+        # Primeiro índice: lamda, Segundo índice: nível de energia
+        autovalores[k, :] = eigvalsh(
             a=H,
-        )
+        )[0:2]
 
-        autovalores[k, :] = w
-        autovetores[k, :, :] = v
+    return autovalores
 
-    return autovalores, autovetores
+
+array_lamda = linspace(
+    start=0.0,
+    stop=10.0,
+    num=1000,
+)
+# save(
+#     file='projeto/array_lamda',
+#     arr=array_lamda,
+# )
+
+
+def salvar_array_autoval(N: int, array_lamda: ndarray = array_lamda):
+    autoval = diagonalizacao_N(
+        N=N,
+        array_lamda=array_lamda
+    )
+    save(
+        file='projeto/autoval_' + str(N),
+        arr=autoval,
+    )
+
+
+for N in arange(2, N_max + 1):
+    autoval = diagonalizacao_N(
+        N=N,
+        array_lamda=array_lamda
+    )
+    save(
+        file='projeto/autoval_' + str(N),
+        arr=autoval[:, 0:2],
+    )

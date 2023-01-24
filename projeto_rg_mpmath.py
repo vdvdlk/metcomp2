a34505ed5fd2690f0572c0d86733077d2a9a4eb2
vdvdlk <thalesfreitas96@gmail.com>
2023-01-24 04:06:50 -0300

    Fiz o gráfico da renormalização em NumPy

--- projeto_rg_mpmath.py
+++ projeto_rg_mpmath.py
@@ -1,10 +1,14 @@
+import pickle
 from math import log2
 
+import numpy as np
 import matplotlib.pyplot as plt
 import mpmath as mp
 from tqdm.auto import tqdm
 
-from cadeia_ising_transverso import indice_base, lista_base, troca_spin_ket, indice_base_par, indice_base_impar
+# from cadeia_ising_transverso import (indice_base, indice_base_impar,
+#                                      indice_base_par, lista_base,
+#                                      troca_spin_ket)
 
 mp.mp.dps = 10**4
 
@@ -80,49 +84,23 @@ def H_j_n(n_s: int, J_n, h_n):
     for p in range(1, n_s + 1):
         matriz_campo += S_jp_z_n(n_s, p)
 
-    H = - (J_n * matriz_int + h_n * matriz_campo)
+    H = - J_n * matriz_int - h_n * matriz_campo
 
     return H
 
 
 def diagonalizacao(H_j_n):
-    E, Q = mp.eigh(H_j_n)
+    E, Q = mp.eigsy(H_j_n)
 
     # Estado fundamental
     E_mais_nmais1 = E[0]
-    ket_mais_nmais1 = Q[:, 0]
+    ket_mais_nmais1 = mp.chop(Q[:, 0])
 
     # Primeiro estado excitado
     E_menos_nmais1 = E[1]
-    ket_menos_nmais1 = Q[:, 1]
+    ket_menos_nmais1 = mp.chop(Q[:, 1])
 
     return E_mais_nmais1, E_menos_nmais1, ket_mais_nmais1, ket_menos_nmais1
-
-
-H = H_j_n(
-    3,
-    mp.mpf('2.0'),
-    mp.mpf('0.2')
-)
-
-# print(H)
-# E_mais, E_menos, vetor_mais, vetor_menos = diagonalizacao(
-#     H_j_n=H
-# )
-# h = (E_menos - E_mais) / 2
-# media = (E_menos + E_mais) / 2
-
-# print(vetor_mais.T * S_jp_z_n(2, 1) * vetor_mais)
-# print(vetor_mais.T * S_jp_z_n(2, 1) * vetor_menos)
-# print(vetor_menos.T * S_jp_z_n(2, 1) * vetor_mais)
-# print(vetor_menos.T * S_jp_z_n(2, 1) * vetor_menos)
-
-# print(vetor_mais)
-# print()
-# print(vetor_menos)
-# print()
-
-# print(vetor_mais[0] * vetor_menos[2] + vetor_mais[3] * vetor_menos[1])
 
 
 def S_j_x_n(ket_mais_n, ket_menos_n):
@@ -140,64 +118,31 @@ def I_j_n(ket_mais_n, ket_menos_n):
     return matriz
 
 
-# print(S_j_z_n(vetor_mais, vetor_menos))
-# print()
-# print(vetor_menos.T * S_j_z_n(vetor_mais, vetor_menos) * vetor_menos)
-
-
 def csi_p_n(ket_mais_nmais1, ket_menos_nmais1, p: int):
     n_s = int(log2(ket_mais_nmais1.rows))
-
-    csi_1 = (ket_mais_nmais1.T * S_jp_x_n(n_s, p) * ket_menos_nmais1)[0, 0]
-
-    return csi_1
+    csi = (ket_mais_nmais1.T * S_jp_x_n(n_s, p) * ket_menos_nmais1)[0, 0]
+    return csi
 
 
-# def csi_p_n(ket_mais_nmais1, ket_menos_nmais1, p: int = 1):
-#     n_s = int(log2(ket_mais_nmais1.rows))
-#     base = lista_base(
-#         N=n_s
-#     )
+# def csi_1_n(ket_mais_n, ket_menos_n):
+#     n_s = int(log2(ket_mais_n.rows))
 #     indices = indice_base_par(N=n_s)
 
 #     csi = 0
 #     for i in indices:
-#         ket = base[i]
-#         ket_trocado = troca_spin_ket(
-#             ket=ket,
-#             p=p
-#         )
-#         i_menos = indice_base(
-#             ket=ket_trocado
-#         )
-#         csi += mp.fmul(
-#             x=ket_mais_nmais1[i],
-#             y=ket_menos_nmais1[i_menos]
-#         )
+#         if i < 2**(n_s - 1):
+#             i_menos = i + 2**(n_s - 1)
+#         else:
+#             i_menos = i - 2**(n_s - 1)
+
+#         csi += ket_mais_n[i] * ket_menos_n[i_menos]
+
+#     # csi = mp.fabs(csi)
 
 #     return csi
 
 
-def csi_1_n(ket_mais_n, ket_menos_n):
-    n_s = int(log2(ket_mais_n.rows))
-    indices = indice_base_par(N=n_s)
-
-    csi = 0
-    for i in indices:
-        if i < 2**(n_s - 1):
-            i_menos = i + 2**(n_s - 1)
-        else:
-            i_menos = i - 2**(n_s - 1)
-
-        csi += mp.fmul(
-            x=ket_mais_n[i],
-            y=ket_menos_n[i_menos]
-        )
-
-    return csi
-
-
-def iteracao(n_s, J_n, h_n):
+def iteracao(n_s: int, J_n, h_n, C_n):
     E_mais_nmais1, E_menos_nmais1, ket_mais_nmais1, ket_menos_nmais1 = diagonalizacao(
         H_j_n=H_j_n(n_s, J_n, h_n)
     )
@@ -211,104 +156,205 @@ def iteracao(n_s, J_n, h_n):
     #     ket_mais_nmais1,
     #     ket_menos_nmais1
     # )
-    csi_1_2 = mp.fmul(
-        x=csi_1,
-        y=csi_1
-    )
 
-    delta_E = mp.fsub(
-        x=E_menos_nmais1,
-        y=E_mais_nmais1
-    )
+    J_nmais1 = csi_1**2 * J_n
+    h_nmais1 = 0.5 * (E_menos_nmais1 - E_mais_nmais1)
+    C_nmais1 = n_s * C_n + 0.5 * (E_mais_nmais1 + E_menos_nmais1)
 
-    J_nmais1 = mp.fmul(
-        x=csi_1_2,
-        y=J_n
-    )
-    h_nmais1 = mp.fmul(
-        x=0.5,
-        y=delta_E
-    )
-
-    return J_nmais1, h_nmais1
+    return J_nmais1, h_nmais1, C_nmais1
 
 
-def renormalizacao(n_s, num_int=50, num_pt=101):
-    fator = mp.mpf('0.001')
+def renormalizacao(n_s, num_int, num_pt=200):
     J_0 = mp.mpf('1.0')
+    C_0 = mp.mpf('0.0')
     lista_h_0 = mp.linspace(
-        0,
+        0.1,
         2 * J_0,
         num_pt
     )
 
     lista_J_n = []
     lista_h_n = []
+    lista_C_n = []
 
-    alcance_array = tqdm(
+    range_tqdm = tqdm(
         range(num_pt),
         desc='Renormalização n_s = ' + str(n_s)
     )
-    alcance_int = range(1, num_int + 1)
-    for r in alcance_array:
-        J_n, h_n = (
-            J_0 * fator,
-            lista_h_0[r] * fator
+
+    for r in range_tqdm:
+        J_n, h_n, C_n = (
+            J_0,
+            lista_h_0[r],
+            C_0
         )
-        for n in alcance_int:
-            J_n, h_n = iteracao(
+        for n in range(1, num_int + 1):
+            J_n, h_n, C_n = iteracao(
                 n_s=n_s,
                 J_n=J_n,
-                h_n=h_n
+                h_n=h_n,
+                C_n=C_n
             )
 
-        J_n = J_n / fator
         lista_J_n.append(J_n)
-
-        h_n = h_n / fator
         lista_h_n.append(h_n)
+        lista_C_n.append(C_n)
 
-    return lista_h_0, lista_J_n, lista_h_n
+    return lista_h_0, lista_J_n, lista_h_n, lista_C_n
 
 
-fig, axs = plt.subplots(
-    ncols=2
-)
-
-axs[0].set_xlabel('$h / J$')
-axs[0].set_ylabel('$J^\\infty / J$')
-
-axs[1].set_xlabel('$h / J$')
-axs[1].set_ylabel('$h^\\infty / J$')
-
-fig.set_size_inches(15, 5)
-
-pasta = 'projeto/renormalizacao/'
-lista_n_s = [2, 3, 4, 5, 6, 7]
-lista_num_int = [10, 6, 6, 6, 6, 6]
-i = 0
-for n_s in lista_n_s:
-    lista_h_0, lista_J_n, lista_h_n = renormalizacao(
+def salvar_listas(n_s: int, num_int: int):
+    lista_h_0, lista_J_n, lista_h_n, lista_C_n = renormalizacao(
         n_s=n_s,
-        num_int=lista_num_int[i],
-        # num_pt=101,
-    )
-    axs[0].plot(
-        lista_h_0,
-        lista_J_n,
-        label='$n_s = ' + str(n_s) + '$'
-    )
-    axs[1].plot(
-        lista_h_0,
-        lista_h_n,
-        label='$n_s = ' + str(n_s) + '$'
+        num_int=num_int,
     )
 
-    axs[0].legend()
-    axs[1].legend()
+    arquivo_h_0 = 'projeto/renormalizacao/lista_h_0_' + str(n_s) + '.pickle'
+    arquivo_J_n = 'projeto/renormalizacao/lista_J_n_' + str(n_s) + '.pickle'
+    arquivo_h_n = 'projeto/renormalizacao/lista_h_n_' + str(n_s) + '.pickle'
+    arquivo_C_n = 'projeto/renormalizacao/lista_C_n_' + str(n_s) + '.pickle'
 
-    fig.savefig(pasta + 'teste.pdf')
+    with open(arquivo_h_0, 'wb') as f:
+        pickle.dump(lista_h_0, f)
 
-    i += 1
+    with open(arquivo_J_n, 'wb') as f:
+        pickle.dump(lista_J_n, f)
 
-# plt.show()
+    with open(arquivo_h_n, 'wb') as f:
+        pickle.dump(lista_h_n, f)
+
+    with open(arquivo_C_n, 'wb') as f:
+        pickle.dump(lista_C_n, f)
+
+
+def graficos(salvar=False, plotar=False):
+    fig, axs = plt.subplots(
+        nrows=2,
+        ncols=2
+    )
+
+    x_0 = np.linspace(0, 2, 1000)
+    y_0 = 1 - x_0**2
+    axs[0][0].plot(
+        x_0,
+        y_0,
+        color='black',
+        label='Resultado exato'
+    )
+
+    x_1 = np.linspace(0, 2, 1000)
+    y_1 = x_1 - 1.0
+    axs[0][1].plot(
+        x_1,
+        y_1,
+        color='black',
+        label='Resultado exato'
+    )
+
+    lista_int = [15, 7, 5, 5, 5, 5]
+    i = 0
+    for n_s in [2, 3, 4]:
+        arquivo_h_0 = 'projeto/renormalizacao/lista_h_0_' + \
+            str(n_s) + '.pickle'
+        arquivo_J_n = 'projeto/renormalizacao/lista_J_n_' + \
+            str(n_s) + '.pickle'
+        arquivo_h_n = 'projeto/renormalizacao/lista_h_n_' + \
+            str(n_s) + '.pickle'
+        arquivo_C_n = 'projeto/renormalizacao/lista_C_n_' + \
+            str(n_s) + '.pickle'
+
+        with open(arquivo_h_0, 'rb') as f:
+            lista_h_0 = pickle.load(f)
+        array_h_0 = np.array(
+            object=lista_h_0,
+            dtype=float
+        )
+        dh = np.diff(array_h_0)[0]
+
+        with open(arquivo_J_n, 'rb') as f:
+            lista_J_n = pickle.load(f)
+
+        with open(arquivo_h_n, 'rb') as f:
+            lista_h_n = pickle.load(f)
+
+        with open(arquivo_C_n, 'rb') as f:
+            lista_C_n = pickle.load(f)
+        array_C_n = np.array(
+            object=lista_C_n,
+            dtype=float
+        )
+
+        array_E_0_N = array_C_n / n_s**lista_int[i]
+        array_dE_0_N = np.gradient(array_E_0_N, dh)
+        array_d2E_0_N = np.gradient(array_dE_0_N, dh)
+
+        axs[0][0].plot(
+            lista_h_0,
+            lista_J_n,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        axs[0][1].plot(
+            lista_h_0,
+            lista_h_n,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        axs[1][0].plot(
+            lista_h_0,
+            array_C_n / n_s**lista_int[i],
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        axs[1][1].plot(
+            lista_h_0,
+            - array_d2E_0_N,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        i += 1
+
+    axs[0][0].set_box_aspect(1)
+    axs[0][0].set_xlabel('$h / J$')
+    axs[0][0].set_ylabel('$J^\\infty / J$')
+    axs[0][0].legend()
+    axs[0][0].set_xlim(0, 2)
+    axs[0][0].set_ylim(0, 1)
+
+    axs[0][1].set_box_aspect(1)
+    axs[0][1].set_xlabel('$h / J$')
+    axs[0][1].set_ylabel('$h^\\infty / J$')
+    axs[0][1].legend()
+    axs[0][1].set_xlim(0, 2)
+    axs[0][1].set_ylim(0, 1)
+
+    axs[1][0].set_box_aspect(1)
+    axs[1][0].set_xlabel('$h / J$')
+    axs[1][0].set_ylabel('$C_n / J$')
+    axs[1][0].legend()
+    axs[1][0].set_xlim(0.9, 1.3)
+    # axs[1][0].set_ylim(0, 3)
+
+    axs[1][1].set_box_aspect(1)
+    axs[1][1].set_xlabel('$h / J$')
+    axs[1][1].set_ylabel('$(E_0 / N)_{N \\to \\infty} = \\chi_z$')
+    axs[1][1].legend()
+    axs[1][1].set_xlim(0.9, 1.3)
+    axs[1][1].set_ylim(0, 3)
+
+    fig.set_size_inches(10, 10)
+
+    if salvar == True:
+        fig.savefig('projeto/apresentacao_final/renorm.pdf')
+
+    if plotar == True:
+        plt.show()
+
+
+if __name__ == '__main__':
+    graficos(salvar=True)
+
+    # salvar_listas(2, 15)
+    # salvar_listas(3, 7)
+    # salvar_listas(4, 5)
+    # salvar_listas(5, 5)
---
+++ projeto_rg_numpy.py
@@ -0,0 +1,344 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.linalg import eigh
+from tqdm.auto import tqdm
+
+# from cadeia_ising_transverso import indice_base, lista_base, troca_spin_ket
+
+S_x = np.array(
+    object=[
+        [0, 1],
+        [1, 0]
+    ],
+    dtype=int,
+)
+S_z = np.array(
+    object=[
+        [1, 0],
+        [0, -1]
+    ],
+    dtype=int,
+)
+I = np.identity(
+    n=2,
+    dtype=int,
+)
+
+
+def S_jp_x_n(n_s: int, p: int):
+    array = np.identity(
+        n=1,
+        dtype=int,
+    )
+
+    for pp in range(1, n_s + 1):
+        if pp == p:
+            array = np.kron(
+                a=array,
+                b=S_x,
+            )
+        else:
+            array = np.kron(
+                a=array,
+                b=I,
+            )
+
+    return array
+
+
+def S_jp_z_n(n_s: int, p: int):
+    array = np.identity(
+        n=1,
+        dtype=int,
+    )
+
+    for pp in range(1, n_s + 1):
+        if pp == p:
+            array = np.kron(
+                a=array,
+                b=S_z,
+            )
+        else:
+            array = np.kron(
+                a=array,
+                b=I,
+            )
+
+    return array
+
+
+def H_j_n(n_s: int, J_n: float, h_n: float):
+    array_int = np.zeros(
+        shape=2 * (2**n_s,),
+        dtype=int
+    )
+    array_campo = array_int.copy()
+
+    for p in range(1, (n_s - 1) + 1):
+        array_int += S_jp_x_n(n_s, p) @ S_jp_x_n(n_s, p + 1)
+
+    for p in range(1, n_s + 1):
+        array_campo += S_jp_z_n(n_s, p)
+
+    H = - J_n * array_int - h_n * array_campo
+
+    return H
+
+
+def diagonalizacao(H_j_n, tol=1e-10):
+    w, v = eigh(
+        a=H_j_n,
+        subset_by_index=[0, 1],
+        driver='evr'
+    )
+
+    # Estado fundamental
+    E_mais_nmais1 = w[0]
+    ket_mais_nmais1 = v[:, 0]
+    ket_mais_nmais1[np.abs(ket_mais_nmais1) < tol] = 0.0
+
+    # Primeiro estado excitado
+    E_menos_nmais1 = w[1]
+    ket_menos_nmais1 = v[:, 1]
+    ket_menos_nmais1[np.abs(ket_menos_nmais1) < tol] = 0.0
+
+    return E_mais_nmais1, E_menos_nmais1, ket_mais_nmais1, ket_menos_nmais1
+
+
+def S_j_x_n(ket_mais_n, ket_menos_n):
+    array = np.outer(
+        a=ket_menos_n,
+        b=ket_mais_n
+    )
+    array += np.outer(
+        a=ket_mais_n,
+        b=ket_menos_n
+    )
+
+    return array
+
+
+def S_j_z_n(ket_mais_n, ket_menos_n):
+    array = np.outer(
+        a=ket_menos_n,
+        b=ket_menos_n
+    )
+    array += - np.outer(
+        a=ket_mais_n,
+        b=ket_mais_n
+    )
+
+    return array
+
+
+def I_j_n(ket_mais_n, ket_menos_n):
+    array = np.outer(
+        a=ket_menos_n,
+        b=ket_menos_n
+    )
+    array += np.outer(
+        a=ket_mais_n,
+        b=ket_mais_n
+    )
+
+    return array
+
+
+def csi_p_n(ket_mais_nmais1, ket_menos_nmais1, p: int):
+    n_s = int(np.log2(ket_mais_nmais1.size))
+
+    csi = ket_mais_nmais1 @ S_jp_x_n(n_s, p) @ ket_menos_nmais1
+
+    return csi
+
+
+def iteracao(n_s: int, J_n: float, h_n: float, C_n: float):
+    E_mais_nmais1, E_menos_nmais1, ket_mais_nmais1, ket_menos_nmais1 = diagonalizacao(
+        H_j_n=H_j_n(n_s, J_n, h_n)
+    )
+
+    csi_1 = csi_p_n(
+        ket_mais_nmais1,
+        ket_menos_nmais1,
+        p=1
+    )
+
+    J_nmais1 = csi_1**2 * J_n
+    h_nmais1 = (0.5) * (E_menos_nmais1 - E_mais_nmais1)
+    C_nmais1 = n_s * C_n + (0.5) * (E_mais_nmais1 + E_menos_nmais1)
+
+    return J_nmais1, h_nmais1, C_nmais1
+
+
+def renormalizacao(n_s: int, num_int: int, num_pt: int = 200):
+    J_0 = 1.0
+    C_0 = 0.0
+    array_h_0 = np.linspace(
+        0.1,
+        2 * J_0,
+        num_pt
+    )
+
+    array_J_n = np.zeros(
+        shape=num_pt,
+        dtype=float,
+    )
+    array_h_n = array_J_n.copy()
+    array_C_n = array_J_n.copy()
+
+    alcance_array = tqdm(
+        range(num_pt),
+        desc='Renormalização n_s = ' + str(n_s)
+    )
+    alcance_int = range(1, num_int + 1)
+    for r in alcance_array:
+        J_n, h_n, C_n = (
+            J_0,
+            array_h_0[r],
+            C_0
+        )
+        for n in alcance_int:
+            J_n, h_n, C_n = iteracao(
+                n_s=n_s,
+                J_n=J_n,
+                h_n=h_n,
+                C_n=C_n
+            )
+
+        array_J_n[r] = J_n
+        array_h_n[r] = h_n
+        array_C_n[r] = C_n
+
+    return array_h_0, array_J_n, array_h_n, array_C_n
+
+
+def salvar_arrays(n_s: int, num_int: int):
+    array_h_0, array_J_n, array_h_n, array_C_n = renormalizacao(
+        n_s=n_s,
+        num_int=num_int,
+    )
+
+    arquivo_h_0 = 'projeto/renormalizacao/lista_h_0_' + str(n_s)
+    arquivo_J_n = 'projeto/renormalizacao/lista_J_n_' + str(n_s)
+    arquivo_h_n = 'projeto/renormalizacao/lista_h_n_' + str(n_s)
+    arquivo_C_n = 'projeto/renormalizacao/lista_C_n_' + str(n_s)
+
+    np.save(
+        file=arquivo_h_0,
+        arr=array_h_0
+    )
+
+    # np.save(
+    #     file=arquivo_J_n,
+    #     arr=array_J_n
+    # )
+
+    np.save(
+        file=arquivo_h_n,
+        arr=array_h_n
+    )
+
+    np.save(
+        file=arquivo_C_n,
+        arr=array_C_n
+    )
+
+
+def grafico():
+    fig, axs = plt.subplots(
+        nrows=2,
+        ncols=2
+    )
+
+    axs[0][0].set_box_aspect(1)
+    axs[0][0].set_xlabel('$h / J$')
+    axs[0][0].set_ylabel('$J^\\infty / J$')
+    axs[0][0].set_xlim(0, 2)
+    axs[0][0].set_ylim(0, 1)
+
+    axs[0][1].set_box_aspect(1)
+    axs[0][1].set_xlabel('$h / J$')
+    axs[0][1].set_ylabel('$h^\\infty / J$')
+    axs[0][1].set_xlim(0, 2)
+    axs[0][1].set_ylim(0, 1)
+
+    axs[1][0].set_box_aspect(1)
+    axs[1][0].set_xlabel('$h / J$')
+    axs[1][0].set_ylabel('$C_n / n_s^n$')
+    axs[1][0].set_xlim(0, 2)
+    # axs[1][0].set_ylim(0, 3)
+
+    axs[1][1].set_box_aspect(1)
+    axs[1][1].set_xlabel('$h / J$')
+    axs[1][1].set_ylabel('$(E_0 / N)_{N \\to \\infty} = \\chi_z$')
+    axs[1][1].set_xlim(0.9, 1.3)
+    axs[1][1].set_ylim(0, 3)
+
+    fig.set_size_inches(10, 10)
+
+    x_0 = np.linspace(0, 1, 1000)
+    y_0 = 1 - x_0**2
+    axs[0][0].plot(
+        x_0,
+        y_0,
+        color='black',
+        label='Resultado exato'
+    )
+
+    x_1 = np.linspace(1, 2, 1000)
+    y_1 = x_1 - 1.0
+    axs[0][1].plot(
+        x_1,
+        y_1,
+        color='black',
+        label='Resultado exato'
+    )
+
+    num_int = 100
+    i = 0
+    for n_s in [2, 3, 4, 5, 6, 7]:
+        array_h_0, array_J_n, array_h_n, array_C_n = renormalizacao(
+            n_s=n_s,
+            num_int=100
+        )
+
+        dh = np.diff(array_h_0)[0]
+        array_E_0_N = array_C_n / n_s**num_int
+        array_dE_0_N = np.gradient(array_E_0_N, dh)
+        array_d2E_0_N = np.gradient(array_dE_0_N, dh)
+
+        axs[0][0].plot(
+            array_h_0,
+            array_J_n,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+        axs[0][1].plot(
+            array_h_0,
+            array_h_n,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        axs[1][0].plot(
+            array_h_0,
+            array_C_n / n_s**num_int,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        axs[1][1].plot(
+            array_h_0,
+            - array_d2E_0_N,
+            label='$n_s = ' + str(n_s) + '$'
+        )
+
+        axs[0][0].legend()
+        axs[0][1].legend()
+        axs[1][0].legend()
+        axs[1][1].legend()
+
+        fig.savefig('projeto/renormalizacao/teste_numpy.pdf')
+
+        i += 1
+
+
+if __name__ == '__main__':
+    grafico()
